3d195c980bbf0a6e70d26ad03358c2a6fb7721fa
Valentin <86162752+ValentinLvrr@users.noreply.github.com>
2021-11-27 12:46:22 +0100

    Add files via upload

---
+++ kprint.py
@@ -0,0 +1,36 @@
+def kprint(word, delay):
+    from time import sleep
+    word = list(word.strip())
+    for letter in word:
+        print(letter, end="", flush=True)
+        sleep(delay)
+    print("")
+
+def kinput(word, delay):
+    from time import sleep
+    word = list(word.strip())
+    for letter in word:
+        print(letter, end="", flush=True)
+        sleep(delay)
+    text = input()
+    return text
+
+"""
+-----------------------------------------------
+
+ ALERT | Spaces are not supported
+
+-----------------------------------------------
+
+ PRINT | Example : kprint("Hello World", 0.05)
+                      |         |          |
+                  fonction    text       delay
+
+-----------------------------------------------
+
+ INPUT | Example : text = kinput("Enter text : " 0.05)
+                     |         |           |
+                  fonction    text       delay
+
+-----------------------------------------------
+"""
